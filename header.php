41b0a6c33881205acbb9b3e3f3d4b52591669e2c
aesqe <aesqe@skyphe.org>
2016-05-30 21:59:35 +0200

    moved navigation into separate file

--- header.php
+++ header.php
@@ -23,33 +23,7 @@
 		<div class="site-logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
 
 		<div class="site-tools">
-			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<a href="#primary-menu-container" class="menu-show" aria-controls="primary-menu" aria-expanded="false">Primary Menu</a>
-				<div class="site-menu-drawer">
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'container_id' => 'primary-menu-container' ) );?>
-					<a href="#" class="menu-hide" aria-controls="primary-menu" aria-expanded="false">X</a>
-					<!-- Begin MailChimp Signup Form -->
-
-					<ul class="follow">
-						<li class="title">Follow us</li>
-						<li><a href="#">on Facebook</a></li>
-						<li><a href="#">on Google+</a></li>
-						<li><a href="#">on Twitter</a></li>
-					</ul>
-
-					<div class="mc_embed_signup">
-						<form action="//systems.us12.list-manage.com/subscribe/post?u=be348cfa062148d17ebff31a7&amp;id=3a96094cda" method="post" class="mc-embedded-subscribe-form validate" name="mc-embedded-subscribe-form" target="_blank" novalidate>
-							<div class="mc_embed_signup_scroll">
-								<label for="mce-EMAIL">Newsletter</label>
-								<input type="email" value="" name="EMAIL" class="email" class="mce-EMAIL" placeholder="write your email" required>
-								<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_be348cfa062148d17ebff31a7_3a96094cda" tabindex="-1" value=""></div>
-								<div class="clear"><input type="submit" value="Subscribe" name="subscribe" class="mc-embedded-subscribe button"></div>
-							</div>
-						</form>
-					</div>
-					<!--End mc_embed_signup-->
-				</div>
-			</nav><!-- #site-navigation -->
+			<?php get_template_part('nav-main'); ?>
 
 			<div id="site-search" class="main-search<?php if( is_search() ){ echo ' active'; } ?>">
 				<button class="search-toggle" aria-controls="primary-search" aria-expanded="false">Search</button>
---
+++ nav-main.php
@@ -0,0 +1,27 @@
+<nav id="site-navigation" class="main-navigation" role="navigation">
+	<a href="#primary-menu-container" class="menu-show" aria-controls="primary-menu" aria-expanded="false">Primary Menu</a>
+	<div class="site-menu-drawer">
+		<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'container_id' => 'primary-menu-container' ) );?>
+		<a href="#" class="menu-hide" aria-controls="primary-menu" aria-expanded="false">X</a>
+		
+
+		<ul class="follow">
+			<li class="title">Follow us</li>
+			<li><a href="#">on Facebook</a></li>
+			<li><a href="#">on Twitter</a></li>
+		</ul>
+
+		<!-- Begin MailChimp Signup Form -->
+		<div class="mc_embed_signup">
+			<form action="//systems.us12.list-manage.com/subscribe/post?u=be348cfa062148d17ebff31a7&amp;id=3a96094cda" method="post" class="mc-embedded-subscribe-form validate" name="mc-embedded-subscribe-form" target="_blank" novalidate>
+				<div class="mc_embed_signup_scroll">
+					<label for="mce-EMAIL">Newsletter</label>
+					<input type="email" value="" name="EMAIL" class="email" class="mce-EMAIL" placeholder="write your email" required>
+					<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_be348cfa062148d17ebff31a7_3a96094cda" tabindex="-1" value=""></div>
+					<div class="clear"><input type="submit" value="Subscribe" name="subscribe" class="mc-embedded-subscribe button"></div>
+				</div>
+			</form>
+		</div>
+		<!--End mc_embed_signup-->
+	</div>
+</nav><!-- #site-navigation -->
